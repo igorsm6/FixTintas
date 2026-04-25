8a12bfa20e71171445fd7b127c37c97d0bc04495
Igor Moreira <igorsouzamoreira@hotmail.com>
2026-04-25 12:00:02 -0300

    Adicionando arquivos para o projeto

---
+++ FixTintas/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixTintas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ FixTintas/Servicos/ClienteServico.cs
@@ -0,0 +1,83 @@
+﻿using FixTintas.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FixTintas.Servicos
+{
+    internal class ClienteServico
+    {
+        public List<Clientes> lista = new List<Clientes>();
+        // só pensando... acho que vou criar um contadorID para gerar um ID automatico, não vou vincular o banco agora
+        // porem acho interessante os clientes ter ID certo ? gerar manual
+
+        private int contadorID = 1;
+
+        //criar né papai
+        public void Adicionar(Clientes clientes)
+        {
+            if (clientes.Nome == "")
+            {
+                Console.WriteLine(("Nome Invalido");
+                return;
+            }
+
+            clientes.Id = contadorID;
+            contadorID++;
+
+            list.Add(clientes);
+
+            Console.WriteLine("Cliente cadastrado!");
+        }
+
+        //será que faz sentido eu criar classes separadas ? bom qualquer coisas da para separar depois
+
+        public void Listar()
+        {
+            //foreach ? ou for ? acho que foreach né tudo em uma linha
+            //aqui eu criei o "cliente"
+
+            foreach (var c in lista)
+            {
+                Console.WriteLine($"ID: {c.Id} | Nome: {c.Nome} | CPF: {c.CPF}");
+            }
+        }
+
+        //faz sentido ter algo para atulizar ? acho que faz né
+
+        public void Atualizar(int id, string novoNome, string novoCpf)
+        {
+            foreach(var c in lista)
+            {
+                if(c.Id == id)
+                {
+                    c.Nome = novoNome;
+                    c.CPF = novoCpf;
+                    Console.WriteLine("Cliente Atualizado !");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Cliente não encontrado!");
+        }
+
+        //fiz o criar atualizar faz sentido o deletar né ?
+
+        public void Remover(int id)
+        {
+            foreach (var c in lista)
+            {
+                if(c.Id == id)
+                {
+                    lista.Remove(c);
+                    Console.WriteLine("Cliente Removido!");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Cliente não encontrado!");
+            //acho que é isso, vou vendo como vai ficar, vamos dar um commit ? opa claro
+        }
+
+    }
+}
